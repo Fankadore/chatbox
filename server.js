4a64b77785e4ff714b5874cf057f093fd605836f
Fankadore <rmackenzie88@gmail.com>
2018-03-02 19:54:11 +0000

    express server

--- server.js
+++ server.js
@@ -1,1 +1,22 @@
-test file
+"use strict";
+
+const express = require('express');
+const app = express();
+
+app.get('/', function(req, res) {
+    res.sendFile(__dirname + '/index.html');
+});
+
+let userId = 999999;
+
+app.get('/login/:userId(\\d{6})', function(req, res) {
+    userId = req.params.userId;
+    res.send(userId + " has logged on.");
+});
+
+const server = app.listen(2000, function() {
+    let host = server.address().address;
+    let port = server.address().port;
+    
+    console.log("Server listening at " + host + ":" + port);
+});
